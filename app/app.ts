ad39bce73a36641d45050fa25a56a984eb0fd564
soubhagya <soubhagya.r.nayak@gmail.com>
2021-01-25 21:16:23 +0530

    added dependency on bootstrap for css

--- app/app.ts
+++ app/app.ts
@@ -19,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/css", express.static(path.join(__dirname, 'css')))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
