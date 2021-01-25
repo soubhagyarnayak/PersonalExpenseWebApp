6cc7eec0bc0d4a538c49b8d01157d0132fd91f5c
soubhagya <soubhagya.r.nayak@gmail.com>
2021-01-25 22:19:01 +0530

    added support for calling expense web service via axios

--- app/routes/expense.ts
+++ app/routes/expense.ts
@@ -1,4 +1,5 @@
 // tslint:disable:no-console
+import Axios , {AxiosResponse, AxiosError} from 'axios';
 import express = require('express')
 export const expenseRouter = express.Router()
 
@@ -11,7 +12,21 @@ expenseRouter.get('/', (req:express.Request, res:express.Response, next:express.
 /* Save an expense item. */
 expenseRouter.post('/submit-expense', (req:express.Request, res:express.Response) => {
     const category = req.body.category
+    const recipient = req.body.recipient
+    const description = req.body.description
+    const amount:number = req.body.amount
     console.info(category);
-    // ...
+    Axios.post('http://localhost:8080/expenses', {
+        category,
+        recipient,
+        description,
+        amount
+      })
+      .then((response:AxiosResponse)=> {
+        console.log(response);
+      })
+      .catch((error:AxiosError)=> {
+        console.log(error);
+      });
     res.end()
 })
